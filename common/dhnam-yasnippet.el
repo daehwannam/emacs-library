f6e920ef2e97ec553639bdc12859b01cf908fbd2
Daehwan Nam <dhnam@postech.ac.kr>
2023-06-24 01:59:05 +0900

    minor path fix

--- common/dhnam-yasnippet.el
+++ common/dhnam-yasnippet.el
@@ -6,7 +6,7 @@
 ;;; https://pragmaticemacs.wordpress.com/2016/01/04/smart-text-templates-with-yasnippet/
 
 (progn
-  (defvar dhnam/yasnippet-snippets-dir "~/.emacs.d/dhnamlib/yasnippet-snippets")
+  (defvar dhnam/yasnippet-snippets-dir (concat dhnam/lib-root-dir "yasnippet-snippets"))
   (add-to-list 'yas-snippet-dirs 'dhnam/yasnippet-snippets-dir))
 
 (provide 'dhnam-yasnippet)
