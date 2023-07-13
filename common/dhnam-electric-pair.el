624006f1f66b4d11e0b93f31b2800f942db5bcb8
Daehwan Nam <dhnam@postech.ac.kr>
2023-07-13 21:45:27 +0900

    electric-pair fix for org-mode not to consider <> pair

--- common/dhnam-electric-pair.el
+++ common/dhnam-electric-pair.el
@@ -2,10 +2,16 @@
 (progn
   ;; https://stackoverflow.com/a/27871987
 
-  (defvar python-electric-pairs '((?' . ?')) "Electric pairs for python-mode.")
-  (defun dhnam/python-add-electric-pairs ()
-    (setq-local electric-pair-pairs (append electric-pair-pairs python-electric-pairs))
+  (defvar dhnam/python-additional-electric-pairs '((?' . ?')) "Electric pairs for python-mode.")
+  (defun dhnam/set-python-electric-pairs ()
+    (setq-local electric-pair-pairs (append electric-pair-pairs dhnam/python-additional-electric-pairs))
     (setq-local electric-pair-text-pairs electric-pair-pairs)))
 
+(progn
+  ;; https://stackoverflow.com/a/69765466
+  (defun dhnam/set-org-electric-pair-inhibit-predicate ()
+    (setq-local electric-pair-inhibit-predicate
+                `(lambda (c)
+                   (if (char-equal c ?<) t (,electric-pair-inhibit-predicate c))))))
 
 (provide 'dhnam-electric-pair)
