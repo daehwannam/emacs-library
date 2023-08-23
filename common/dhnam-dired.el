63d655baabca22d335932053d720f9cbbb69c39f
Daehwan Nam <dhnam@postech.ac.kr>
2023-08-24 02:09:28 +0900

    dhnam/dired-rsync update

--- common/dhnam-dired.el
+++ common/dhnam-dired.el
@@ -1,62 +1,95 @@
 
 (progn
-  ;; Copy files asynchronously
+  ;; Copy files asynchronously and remotely via rsync
   ;;
-  ;; https://stackoverflow.com/questions/379940/dired-copy-asynchronously
-  ;; https://oremacs.com/2016/02/24/dired-rsync/
+  ;; Reference:
+  ;; - https://stackoverflow.com/questions/379940/dired-copy-asynchronously
+  ;; - https://oremacs.com/2016/02/24/dired-rsync/
 
   (require 'dired-x)
 
-  (defun dhnam/dired-rsync-run (dest-path &optional max-size)
-    (let ((default-args (list "-rlpt"
-                              "--info=progress2" "--no-i-r"))
+  (defvar dhnam/dired-rsync/default-args
+    (list "-rlptP"
+          "--info=progress2"
+          ;; "--no-i-r"
+          ))
+
+  (defvar dhnam/dired-rsync/displaying-in-current-window t)
+
+  (defvar dhnam/dired-rsync/buffer-name "*rsync*")
+
+  (defun dhnam/dired-rsync/run (dest-path &optional max-size)
+    "Run rsync to copy marked files to DEST-PATH.
+The rsync command can transfer files in the directions of
+local-to-local, local-to-remote, remote-to-local and remote-to-remote.
+This function would not work for multi-hop SSH connections.
+"
+    (let ((default-args dhnam/dired-rsync/default-args)
           (marked-files (dired-get-marked-files
                          nil current-prefix-arg))
+
+          (source-ssh-port-arg nil)
+          (dest-ssh-port-arg nil)
+
+          (dest-as-default-dir nil)
+
+          (source-path-args nil)
+          (dest-path-arg nil)
+
           (ssh-port-arg nil)
-          (max-size-arg nil)
-          (source-args nil)
-          (dest-arg nil)
-          (remote-source-p nil))
+          (max-size-arg nil))
 
-      (let ((source-info-list (mapcar 'dhnam/dired-rsync-extract-args-from-path marked-files))
-            (dest-info (dhnam/dired-rsync-extract-args-from-path dest-path)))
+      (let ((source-info-list (mapcar 'dhnam/dired-rsync/extract-info-from-path marked-files))
+            (dest-info (dhnam/dired-rsync/extract-info-from-path dest-path)))
 
-        (let ((source-hosts (mapcar (lambda (x) (nth 1 x)) source-info-list))
-              (dest-host (nth 1 dest-info)))
-          (if (cl-every (lambda (source-host) (equal source-host dest-host)) source-hosts)
+        (cl-flet ((assocv (key alist) (cdr (assoc key alist))))
+          (let ((source-hosts (mapcar (lambda (x) (assocv 'host-address x)) source-info-list))
+                (dest-host (assocv 'host-address dest-info)))
+            (if (cl-every (lambda (source-host) (equal source-host dest-host)) source-hosts)
+                (progn
+                  ;; Both source and destination are in the same machine, then ssh is unnecessary
+                  (setq source-path-args (mapcar (lambda (x) (assocv 'quoted-local-path x)) source-info-list))
+                  (setq dest-path-arg (assocv 'quoted-local-path dest-info)))
               (progn
-                ;; Both source and destination in the same machine, so ssh is unnecessary
-                (setq source-args (mapcar (lambda (x) (nth 2 x)) source-info-list))
-                (setq dest-arg (nth 2 dest-info)))
-            (progn
-              (progn
-                (setq source-args (mapcar 'car source-info-list))
-                (setq ssh-port-arg (nth 3 (car source-info-list)))
-                (when ssh-port-arg
-                  (setq remote-source-p t)))
+                (progn
+                  (setq dest-path-arg (assocv 'quoted-full-path dest-info))
+                  (setq dest-ssh-port-arg (assocv 'ssh-port-arg dest-info)))
 
-              (progn
-                (setq dest-arg (car dest-info))
-                (unless ssh-port-arg
-                  (setq ssh-port-arg (nth 3 dest-info))))))))
+                (progn
+                  (setq source-ssh-port-arg (assocv 'ssh-port-arg (car source-info-list)))
+                  (let ((source-path-args-key 'quoted-full-path))
+                    (if dest-ssh-port-arg
+                        (progn
+                          (setq ssh-port-arg dest-ssh-port-arg)
+                          (when source-ssh-port-arg
+                            (setq source-path-args-key 'quoted-local-path)))
+                      (when source-ssh-port-arg
+                        (setq ssh-port-arg source-ssh-port-arg)
+                        (setq dest-as-default-dir t)))
+                    (setq source-path-args (mapcar (lambda (x) (assocv source-path-args-key x)) source-info-list)))))))))
 
       (when max-size
         (setq max-size-arg (format "--max-size=%s" max-size)))
 
-      (let ((default-directory (if remote-source-p dest-path default-directory)))
-        (async-shell-command
-         (s-join " " (append (list "rsync")
-                             default-args
-                             (list ssh-port-arg)
-                             (list max-size-arg)
-                             source-args
-                             (list dest-arg)))
-         "*rsync*"))
+      (let ((default-directory (if dest-as-default-dir dest-path default-directory)))
+        (let ((rsync-command (s-join " " (append (list "rsync")
+                                                 default-args
+                                                 (list ssh-port-arg)
+                                                 (list max-size-arg)
+                                                 source-path-args
+                                                 (list dest-path-arg)))))
+          (cl-flet ((run-rsync-command () (async-shell-command rsync-command dhnam/dired-rsync/buffer-name)))
+            (if dhnam/dired-rsync/displaying-in-current-window
+                (let ((display-buffer-alist
+                       (cons (list dhnam/dired-rsync/buffer-name 'display-buffer-same-window)
+                             display-buffer-alist)))
+                  (run-rsync-command))
+              (progn
+                (run-rsync-command)
+                ;; switch to the window of the command buffer
+                (other-window 1))))))))
 
-      ;; switch to the window of *rsync*
-      (other-window 1)))
-
-  (defun dhnam/dired-rsync-extract-args-from-path (path)
+  (defun dhnam/dired-rsync/extract-info-from-path (path)
     (if (tramp-tramp-file-p path)
         (let ((tstruct (tramp-dissect-file-name path)))
           (let ((port (or (tramp-file-name-port-or-default tstruct) 22)))
@@ -67,14 +100,16 @@
                 (local-path (tramp-file-name-localname tstruct)))
 
             (let ((quoted-local-path (shell-quote-argument local-path)))
-              (list (format "%s@%s:%s" user-name host-address quoted-local-path)
-                    host-address
-                    quoted-local-path
-                    ssh-port-arg))))
-      (let ((quoted-path (shell-quote-argument path)))
-        (list quoted-path nil quoted-path nil))))
+              (list
+               (cons 'quoted-full-path (format "%s@%s:%s" user-name host-address quoted-local-path))
+               (cons 'host-address host-address)
+               (cons 'quoted-local-path quoted-local-path)
+               (cons 'ssh-port-arg ssh-port-arg)))))
+      (let ((quoted-local-path (shell-quote-argument path)))
+        (list (cons 'quoted-full-path quoted-local-path)
+              (cons 'quoted-local-path quoted-local-path)))))
 
-  (defun dhnam/dired-rsync (dest-path)
+  (defun dhnam/dired-rsync/do (dest-path)
     (interactive
      (list
       (expand-file-name
@@ -82,11 +117,11 @@
 	    "Rsync to: "
 	    (dired-dwim-target-directory)))))
 
-    (dhnam/dired-rsync-run dest-path))
+    (dhnam/dired-rsync/run dest-path))
 
-  (defvar dhnam/dired-rsync-max-size-history nil)
+  (defvar dhnam/dired-rsync/max-size-history nil)
 
-  (defun dhnam/dired-rsync-with-max-size (dest-path max-size)
+  (defun dhnam/dired-rsync/do-with-max-size (dest-path max-size)
     ;; e.g.
     ;; - dest-path: /ssh:user@address.com#22:/path/to/dir
     ;; - max-size: 500k, 100m, 2g, ...
@@ -96,8 +131,8 @@
        (read-file-name
 	    "Rsync to: "
 	    (dired-dwim-target-directory)))
-      (read-string "Max size: " nil 'dhnam/dired-rsync-max-size-history)))
+      (read-string "Max size: " nil 'dhnam/dired-rsync/max-size-history)))
 
-    (dhnam/dired-rsync-run dest-path max-size)))
+    (dhnam/dired-rsync/run dest-path max-size)))
 
 (provide 'dhnam-dired)
